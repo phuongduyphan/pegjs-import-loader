2a36695500cbe17ff5f91dca70d538569469e9bd
Puyb <stephane.puybareau@commandersact.com>
2022-12-12 17:49:03 +0100

    Fix https://github.com/phuongduyphan/pegjs-import-loader/issues/3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const PegImportHelper = require('./pegImportHelper');
 
 module.exports = function (source, map) {
   const rootPath = this.resourcePath;
-  const options = getOptions(this);
+  const options = getOptions(this) || {};
   options.output = 'source';
 
   const pegHelper = new PegImportHelper(this);
